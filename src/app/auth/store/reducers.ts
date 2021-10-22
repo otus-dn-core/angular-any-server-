4280b0adb5e8eadb2c664162d4a69f32b1f4b93d
otus-dn-core <otus@dn-core.ru>
2021-10-22 22:07:23 +0300

    17 UPDATE Reducers of Authorisation

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,10 +1,13 @@
 import {createReducer, on, Action} from '@ngrx/store'
 
 import {AuthStateInterface} from 'src/app/auth/types/authState.interface'
-import {registerAction} from 'src/app/auth/store/actions/register.action'
+import {registerAction, registerFailureAction, registerSuccessAction} from 'src/app/auth/store/actions/register.action'
 
 const initialState: AuthStateInterface = {
-  isSubmitting: false
+  isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationErrors: null
 }
 
 const authReducer = createReducer(
@@ -13,7 +16,25 @@ const authReducer = createReducer(
     registerAction,
     (state): AuthStateInterface => ({
       ...state,
-      isSubmitting: true
+      isSubmitting: true,
+      validationErrors: null
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors
     })
   )
 )
